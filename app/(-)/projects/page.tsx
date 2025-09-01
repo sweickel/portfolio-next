6e18ddc632321645d78384b9a978e8ef4e07b35f
Sean Weickel <sweickel@gmail.com>
2025-09-01 00:08:48 -0400

    cleanup

--- app/(-)/projects/page.tsx
+++ app/(-)/projects/page.tsx
@@ -1,9 +1,8 @@
 import { getProjects } from "@/app/lib/data";
-import { Project } from "@/app/lib/interfaces/project";
 import ProjectCard from "@/app/ui/(-)/ProjectCard";
 
 export default async function Page() {
-  const projects: Project[] = await getProjects(); // TODO: replace this with call to db to getProjects()
+  const projects = await getProjects();
   return (
     <section className="py-20 bg-background pt-16">
       <div className="container mx-auto px-4">
